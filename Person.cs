253d39911286286f175ffb44b1070af1614e5427
allexeyshestakov <131697227+allexeyshestakov@users.noreply.github.com>
2023-10-10 17:42:31 +0500

    Добавьте файлы проекта.

---
+++ Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace SharpStudyPr
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public virtual void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Возраст: {age}");
+        }
+    }
+
+    public class Student : Person
+    {
+        public string group { get; set; }
+        public int point { get; set; }
+
+        public Student(string name, int age, string group, int point) : base(name, age)
+        {
+            this.group = group;
+            this.point = point;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Группа: {group}");
+            Console.WriteLine($"Оценка: {point}");
+        }
+
+
+    }
+
+    public class Teacher : Rab
+    {
+        public string subject { get; set; }
+
+        public Teacher(string name, int age, string subject, string num, int st) : base(name, age, num, st)
+        {
+            this.subject = subject;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Предмет: {subject}");
+        }
+    }
+
+    public class Rab : Person
+    {
+        public string num { get; set; }
+        public int st { get; set; }
+
+        public Rab(string name, int age, string num, int st) : base(name, age)
+        {
+            this.num = num;
+            this.st = st;
+        }
+
+        public override void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine($"Отдел: {num}");
+            Console.WriteLine($"Стаж: {st}");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace SharpStudyPr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество студентов");
+
+            int countStudents = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество учителей:");
+
+            int countTeachers = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите количество работников:");
+
+            int countRab = int.Parse(Console.ReadLine());
+
+
+
+
+            Person[] people = new Person[countStudents + countTeachers + countRab];
+
+
+            for (int i = 0; i < countStudents; i++)
+            {
+                Console.WriteLine("Введите имя студента");
+                string nameS = Console.ReadLine();
+                Console.WriteLine("Введите возраст студента");
+                int ageS = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите номер группы студента");
+                string grS = "Группа: " + int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите оценка студента");
+                int pointS = int.Parse(Console.ReadLine());
+                Student student = new Student(nameS, ageS, grS, pointS);
+                people[i] = student;
+                student.ShowInfo();
+            }
+            for (int i = countStudents; i < countTeachers + countStudents; i++)
+            {
+                Console.WriteLine("Введите имя преподователя");
+                string nameT = Console.ReadLine();
+                Console.WriteLine("Введите возраст преподователя");
+                int ageT = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите номер группы преподователя");
+                string grT = "Группа: " + int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите стаж работника");
+                int stT = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите предмет преподователя");
+                string suT = "предмет: " + Console.ReadLine();
+                Teacher teacher = new Teacher(nameT, ageT, suT, grT, stT);
+                people[i] = teacher;
+                teacher.ShowInfo();
+            }
+
+            for (int i = countTeachers + countStudents; i < countRab + countTeachers + countStudents; i++)
+            {
+                Console.WriteLine("Введите имя работника");
+                string nameR = Console.ReadLine();
+                Console.WriteLine("Введите возраст работника");
+                int ageR = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите вакансию");
+                string vakR = Console.ReadLine();
+                Console.WriteLine("Введите стаж работника");
+                int stR = int.Parse(Console.ReadLine());
+
+                Rab rab = new Rab(nameR, ageR, vakR, stR);
+                people[i] = rab;
+                rab.ShowInfo();
+            }
+
+            Student student1 = new Student("Студент", 17, "ыы", 3);
+
+            foreach (Person person in people)
+            {
+                student1 = person as Student;
+                if (student1 != null)
+                    if (student1.point == 5)
+                        person.ShowInfo();
+            }
+
+            Rab rab1 = new Rab("Работник", 2, "ewfw", 2);
+
+            foreach (Person person in people)
+            {
+                rab1 = person as Rab;
+                if (rab1 != null)
+                    if (rab1.st > 10)
+                        person.ShowInfo();
+            }
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
